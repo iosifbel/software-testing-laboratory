ccc92d7089bd00a214f59f8559516b36b1c22353
iosifbel <iosifbel@users.noreply.github.com>
2021-03-04 13:21:26 +0200

    deleted Utils and split in readers and universal reader

---
+++ Seminar2/src/ro/ase/cts/clase/UniversalReader.java
@@ -0,0 +1,6 @@
+package ro.ase.cts.clase;
+
+public class UniversalReader {
+    //TO DO
+// punct de legatura catre readere
+}
--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -1,19 +1,20 @@
 package ro.ase.cts.program;
 
-import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.EmployeeReader;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
+
 public class Program {
 
     public static void main(String[] args) {
-        List<Angajat> listaAngajati;
+        List<Aplicant> listaAplicanti;
         try {
-            listaAngajati = Utils.readAngajati("angajati.txt");
-            for(Angajat angajat:listaAngajati)
-                System.out.println(angajat.toString());
+            listaAplicanti = EmployeeReader.readAngajati("angajati.txt");
+            for(Aplicant aplicant:listaAplicanti)
+                System.out.println(aplicant.toString());
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
