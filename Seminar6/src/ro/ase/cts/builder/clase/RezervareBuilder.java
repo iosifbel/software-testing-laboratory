528891c8eab88c84d7f52a1289f90f1e56ee487d
iosifbel <iosifbel@users.noreply.github.com>
2021-04-01 12:25:54 +0300

    RezervareBuilder 1st Generation

---
+++ Seminar6/src/ro/ase/cts/builder/clase/RezervareBuilder.java
@@ -0,0 +1,46 @@
+package ro.ase.cts.builder.clase;
+
+public class RezervareBuilder implements BuilderAbstract {
+
+    private Rezervare rezervare;
+
+    @Override
+    public Rezervare build() {
+        return rezervare;
+    }
+
+    public RezervareBuilder() {
+        super();
+        rezervare = new Rezervare(0,false,false,false,false, "");
+    }
+
+    public RezervareBuilder setCodRezervare(int codRezervare) {
+        rezervare.setCodRezervare(codRezervare);
+        return this;
+    }
+
+    public RezervareBuilder setAreMancareInclusa(boolean areMancareInclusa) {
+        rezervare.setAreMancareInclusa(areMancareInclusa);
+        return this;
+    }
+
+    public RezervareBuilder setAreBauturaInclusa(boolean areBauturaInclusa) {
+        rezervare.setAreBauturaInclusa(areBauturaInclusa);
+        return this;
+    }
+
+    public RezervareBuilder setAreScaunErgonomic(boolean areScaunErgonomic) {
+        rezervare.setAreScaunErgonomic(areScaunErgonomic);
+        return this;
+    }
+
+    public RezervareBuilder setAreMuzicaAmbientala(boolean areMuzicaAmbientala) {
+        rezervare.setAreMuzicaAmbientala(areMuzicaAmbientala);
+        return this;
+    }
+
+    public RezervareBuilder setGenMuzica(String genMuzica) {
+        rezervare.setGenMuzica(genMuzica);
+        return this;
+    }
+}
